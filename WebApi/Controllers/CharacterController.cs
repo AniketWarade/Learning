60dd4a51ba50a2d213f4bc94007b7594c01c7fe9
Aniket Varade <Aniket.Varade@ingrammicro.com>
2021-08-09 12:06:39 +0530

    GET Method Implementation

---
+++ WebApi/Controllers/CharacterController.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        public static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character{Id = 1, Name = "Sam"}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get(){
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id){
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
